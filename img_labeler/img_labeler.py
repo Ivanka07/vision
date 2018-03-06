2e18d6b7c89398ddaff2732e8193a447f8d3fa0c
Ivanna Mykhalchyshyna <ivannamyckhal@uni-koblenz.de>
2018-03-06 21:44:24 +0100

    added buttons

--- img_labeler/img_labeler.py
+++ img_labeler/img_labeler.py
@@ -2,17 +2,72 @@
 
 from tkinter import *
 
-root = Tk()
+class ImageLabeler(Frame):
 
-label_1 = Label(root, text = "Hier kommen die Bilder")
-label_2 = Label(root, text = "Hier kommen die Labels")
+    def __init__(self, width=400, height=400, master = None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.width = width
+        self.height = height
 
-canvas = Canvas(root,width=900, height=700)
-img = PhotoImage(file="/home/ivanna/no-image.png")
-canvas.create_image(20,20, anchor=NW, image=img)	
+        self.init_window()
 
-label_1.grid(row=0, column=0, sticky=N)
-label_2.grid(row=0, column = 1, sticky=N)
-canvas.grid(row=1,column =0 )
+    def init_window(self):
+        self.master.title('Image labeler')
+        self.master.geometry('{}x{}'.format(self.width, self.height))
+        self.master.configure(background='#F8ECE0')
 
-root.mainloop()
+        # layout all of the main containers
+        self.master.grid_rowconfigure(0, weight=1)
+        self.master.grid_rowconfigure(1, weight=1)
+        self.master.grid_rowconfigure(2, weight=6)
+        self.master.grid_rowconfigure(3, weight=1)
+
+        self.master.grid_columnconfigure(0, weight=2)
+        self.master.grid_columnconfigure(1, weight=1)
+        label_1 = Label(self.master, text = "Hier kommen die Bilder", background='#F8ECE0')
+        label_2 = Label(self.master, text = "Hier kommen die Labels",background='#F8ECE0')
+
+        label_1.grid(row = 1, column = 0)
+        label_2.grid(row=1, column=1)
+
+        buttons_frame = Frame(self.master, background='#F8ECE0',pady=1)
+        buttons_frame.grid(row=4, column=0, sticky="ew")
+
+        buttons_frame.grid_columnconfigure(0, weight=1)
+        buttons_frame.grid_columnconfigure(1, weight=1)
+
+
+#        canvas_frame = Frame(self.master, background='#F5BCA9', width=int(self.width*0.45), height=self.height*0.85, pady=1)
+ #       canvas_frame.grid(row = 2, column=0, sticky="ew")
+
+        canvas = Canvas(self.master, width = int(self.width*0.75), height=self.height*0.85)
+        img = PhotoImage(file="/home/ivanna/motion_planning_handle.png")
+        print(img.height())
+        canvas.create_image(10, 10, image=img)
+        canvas.grid(row = 2, column=0)
+
+        self.init_buttons(buttons_frame)
+
+
+    def init_buttons(self, frame):
+        but_left = Button(frame, text='prev image', bg='#92C05D', width=25, height=2, command=self.get_prev_image)
+        but_left.grid(row = 0, column = 0)
+
+        but_rigth = Button(frame, text='next image', bg='#92C05D', width=25, height=2, command=self.get_next_image)
+        but_rigth.grid(row=0, column=1)
+
+
+
+
+    def get_prev_image(self):
+        print('Getting previous image')
+
+    def get_next_image(self):
+        print('Getting next image')
+
+
+if __name__ == '__main__':
+    root = Tk()
+    im = ImageLabeler(1500, 900, root)
+    root.mainloop()
